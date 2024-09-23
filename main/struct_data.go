3c718ac7964ea020e38da941ad54022abe7546d1
任意のなめこ <worktask1124@outlook.com>
2024-09-23 15:56:26 +0900

    commit2

---
+++ main/struct_data.go
@@ -0,0 +1,13 @@
+package main
+
+type Animal struct {
+	zootype    string
+	order      map[string]string
+	suborder   map[int32]string
+	infraorder map[int16]string
+	parvorder  map[int8]string
+	large      float32
+	hight      float32
+	heavy      float32
+	lifespana  float32
+}
